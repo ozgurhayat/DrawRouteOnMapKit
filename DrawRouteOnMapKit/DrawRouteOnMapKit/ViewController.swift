9cd8431258924360d015094bc4857ca3ba2fdcf0
Ozgur Hayat <54996212+ozgurhayat@users.noreply.github.com>
2020-10-21 22:08:05 +0200

    Add files via upload

---
+++ DrawRouteOnMapKit/DrawRouteOnMapKit/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  DrawRouteOnMapKit
+//
+//  Created by Ozgur Hayat on 19/10/2020.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude:38.4237 , longitude: 27.1428)
+        let destinationLocation = CLLocationCoordinate2D(latitude:41.0082 , longitude: 28.9784)
+        
+        createPath(sourceLocation: sourceLocation, destinationLocation: destinationLocation)
+        
+        self.mapView.delegate = self
+    }
+
+    func createPath(sourceLocation : CLLocationCoordinate2D, destinationLocation : CLLocationCoordinate2D) {
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        
+        let sourceMapItem = MKMapItem(placemark: sourcePlaceMark)
+        let destinationItem = MKMapItem(placemark: destinationPlaceMark)
+        
+        
+        let sourceAnotation = MKPointAnnotation()
+        sourceAnotation.title = "Izmir"
+        if let location = sourcePlaceMark.location {
+            sourceAnotation.coordinate = location.coordinate
+        }
+        
+        let destinationAnotation = MKPointAnnotation()
+        destinationAnotation.title = "Istanbul"
+        if let location = destinationPlaceMark.location {
+            destinationAnotation.coordinate = location.coordinate
+        }
+        
+        self.mapView.showAnnotations([sourceAnotation, destinationAnotation], animated: true)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationItem
+        directionRequest.transportType = .automobile
+        
+        let direction = MKDirections(request: directionRequest)
+        
+        
+        direction.calculate { (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print("error : \(error.localizedDescription)")
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+            
+        }
+    }
+
+}
+
+extension ViewController : MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let rendere = MKPolylineRenderer(overlay: overlay)
+        rendere.lineWidth = 5
+        rendere.strokeColor = .systemBlue
+        
+        return rendere
+    }
+}
+
